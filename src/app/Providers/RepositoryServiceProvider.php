6acac611b9826c848c707d715b6c249fb5dae6af
exododigitalmkt <exododigitalmkt@gmail.com>
2021-05-13 16:32:31 -0300

    sanctum add

--- src/app/Providers/RepositoryServiceProvider.php
+++ src/app/Providers/RepositoryServiceProvider.php
@@ -4,12 +4,14 @@ namespace App\Providers;
 
 use App\Repositories\Contracts\{
     CategoryRepositoryInterface,
+    ClientRepositoryInterface,
     ProductRepositoryInterface,
     TableRepositoryInterface,
     TenantRepositoryInterface,
 };
 use App\Repositories\{
     CategoryRepository,
+    ClientRepository,
     ProductRepository,
     TableRepository,
     TenantRepository,
@@ -40,6 +42,10 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(
             ProductRepositoryInterface::class,
             ProductRepository::class,
+        );
+        $this->app->bind(
+            ClientRepositoryInterface::class,
+            ClientRepository::class,
         );
     }
 
--- src/routes/api.php
+++ src/routes/api.php
@@ -6,8 +6,26 @@ use App\Http\Controllers\Api\{
     ProductController,
     TableController,
 };
+use App\Http\Controllers\Api\Auth\{
+    AuthClientController,
+    RegisterController,
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'namespace' => 'Api'
+], function () {
+    Route::post('sanctum/token', [AuthClientController::class, 'auth']);
+});
+
+Route::group([
+    'namespace' => 'Api',
+    'middleware' => ['auth:sanctum']
+], function () {
+    Route::get('auth/me', [AuthClientController::class, 'me']);
+    Route::post('auth/logout', [AuthClientController::class, 'logout']);
+});
 
 Route::group([
     'prefix' => 'v1',
@@ -24,4 +42,7 @@ Route::group([
 
     Route::get('products/{flag}', [ProductController::class, 'show']);
     Route::get('products', [ProductController::class, 'getProductsByTenant']);
+
+    Route::post('client', [RegisterController::class, 'store']);
+
 });
